fc99671b52c5622e534f2075529682f6c3860427
Quanuanc <quanuanc@outlook.com>
2026-01-18 18:42:59 +0800

    Initial Commit

---
+++ Counta/CountaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountaApp.swift
+//  Counta
+//
+//  Created by quanuanc on 2026/1/18.
+//
+
+import SwiftUI
+
+@main
+struct CountaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
